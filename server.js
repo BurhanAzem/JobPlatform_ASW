c9d67b338c8b7fdf30ceff94796c9f33c5c39557
BurhanAzem <104472357+BurhanAzem@users.noreply.github.com>
2023-05-25 19:19:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const http = require("http");
-<<<<<<< HEAD
-=======
+
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger.js'); // Update the path according to your Swagger configuration file
 
->>>>>>> 8fcfa5a01c02b26b4e2e44134faade0059426f1f
+
 const app = require("./app");
 const port = 3003;
 //app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
